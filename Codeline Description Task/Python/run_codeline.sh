c0912c283fe4b662b03dd416b076b647b03cdd52
Brian <30914529+Brianzhengca@users.noreply.github.com>
2024-12-28 17:31:08 -0800

    Update run_codeline.sh

--- Codeline Description Task/Python/run_codeline.sh
+++ Codeline Description Task/Python/run_codeline.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#SBATCH --partition=gpu-l40s
-#SBATCH --account=ark
-#SBATCH --mem-per-gpu=28G
-#SBATCH --cpus-per-gpu=5
-#SBATCH --gres=gpu:1
-#SBATCH --time=23:00:00
-#SBATCH --job-name=run_codeline
-#SBATCH --output="run_codeline.out"
-
 conda init
 conda activate tokenizer-robustness
 
 python generate_codeline.py
+python evaluate_codeline.py
