0da9bb4800970d99d199350a20fcfece8d285634
Brian <30914529+Brianzhengca@users.noreply.github.com>
2024-12-28 17:27:13 -0800

    Update run_codeline.sh

--- Codeline Description Task/Java/run_codeline.sh
+++ Codeline Description Task/Java/run_codeline.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#SBATCH --partition=gpu-a100
-#SBATCH --account=cse
-#SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=5
-#SBATCH --gres=gpu:1
-#SBATCH --time=23:00:00
-#SBATCH --job-name=run_codeline
-#SBATCH --output="run_codeline.out"
-
 conda init
 conda activate tokenizer-robustness
 
 python generate_codeline.py
+python evaluate_codeline.py
