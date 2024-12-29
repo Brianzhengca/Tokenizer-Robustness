42969cc0f9d600ee579e1083fa45f9dfb7d9017c
Brian <30914529+Brianzhengca@users.noreply.github.com>
2024-12-28 17:51:15 -0800

    Update run_gsm.sh

--- General Benchmarks/GSM/run_gsm.sh
+++ General Benchmarks/GSM/run_gsm.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#SBATCH --partition=gpu-l40s
-#SBATCH --account=ark
-#SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=5
-#SBATCH --gres=gpu:1
-#SBATCH --time=3-00:00:00
-#SBATCH --job-name=run_gsm
-#SBATCH --output="run_gsm.out"
-
 conda init
 conda activate tokenizer-robustness
 
 python generate.py
+python evaluate_gsm.py
