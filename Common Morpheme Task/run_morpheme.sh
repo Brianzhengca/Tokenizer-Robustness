b6a5fc42caf7d4a75eac63d6ea8c8f93c5cb5a55
Brian <30914529+Brianzhengca@users.noreply.github.com>
2024-12-28 17:32:47 -0800

    Update run_morpheme.sh

--- Common Morpheme Task/run_morpheme.sh
+++ Common Morpheme Task/run_morpheme.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#SBATCH --partition=gpu-l40
-#SBATCH --account=ark
-#SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=5
-#SBATCH --gres=gpu:1
-#SBATCH --time=23:00:00
-#SBATCH --job-name=run_morpheme
-#SBATCH --output="run_morpheme.out"
-
 conda init
 conda activate tokenizer-robustness
 
 python generate_morpheme.py
+python evaluate_morpheme.py
