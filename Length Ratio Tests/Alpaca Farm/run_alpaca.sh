0e8a71129a15e42aff93ec6fc5da233d04a3ccfa
Brian <30914529+Brianzhengca@users.noreply.github.com>
2024-12-28 18:03:49 -0800

    Update run_alpaca.sh

--- Length Ratio Tests/Alpaca Farm/run_alpaca.sh
+++ Length Ratio Tests/Alpaca Farm/run_alpaca.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#SBATCH --partition=gpu-l40s
-#SBATCH --account=ark
-#SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=5
-#SBATCH --gres=gpu:1
-#SBATCH --time=3-00:00:00
-#SBATCH --job-name=run_alpaca
-#SBATCH --output="run_alpaca.out"
-
 conda init
 conda activate tokenizer-robustness
 
 python generate.py
+python evaluate_alpaca.py
