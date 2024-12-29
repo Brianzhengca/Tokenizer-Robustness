b041d6db7e465bebd2ca01a3c9e8b26af253a21d
Brian <30914529+Brianzhengca@users.noreply.github.com>
2024-12-28 17:09:53 -0800

    Update generate_promptfile.py

--- General Benchmarks/MATH/generate_promptfile.py
+++ General Benchmarks/MATH/generate_promptfile.py
@@ -1,9 +1,10 @@
 import json
+import random
 from evaluation_utils import normalize_final_answer, remove_boxed, last_boxed_only_string
 
 promptfile = open("math_prompts.txt", "w")
 groundtruthfile = open("math_groundtruth.txt", "w")
-prompts = [json.loads(line) for line in open("test.jsonl", "r").readlines()]
+prompts = random.sample([json.loads(line) for line in open("test.jsonl", "r").readlines()], 800)
 
 def prune_solution(full_solution):
     return normalize_final_answer(remove_boxed(last_boxed_only_string((full_solution))))
