4ca4dfb689791f66355fb120f6a1f80afa007e8a
Brian <30914529+Brianzhengca@users.noreply.github.com>
2025-06-22 19:46:57 -0400

    Delete Codeline Description Task directory

--- Codeline Description Task/Javascript/run_codeline.sh
+++
@@ -1,5 +0,0 @@
-conda init
-conda activate tokenizer-robustness
-
-python generate_codeline.py
-python evaluate_codeline.py
--- Codeline Description Task/README.md
+++
@@ -1,3 +0,0 @@
-# Instructions to Run
-1) Change into the directory of the task
-2) ```source run_codeline.sh```
